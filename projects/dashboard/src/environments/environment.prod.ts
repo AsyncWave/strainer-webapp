7e522e64a9c87a1b17e175e5aed84f1bb6ad820c
Malith-Wijesekara <154170x@uom.lk>
2020-01-16 15:33:45 +0530

    Profile added

--- projects/dashboard/src/environments/environment.prod.ts
+++ projects/dashboard/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   nishanTwitterApiUrl: 'https://strainer-twitter-server.herokuapp.com/',
-  nishanApiUrl: 'https://strainer-rest-api.herokuapp.com/'
+  
+  nishanApiUrl: 'https://strainer-rest-api.herokuapp.com/',
+  malithApiUrl: 'https://strainer-credibility-analyzer.herokuapp.com/'
 };
