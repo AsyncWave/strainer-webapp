4a58030f2fe4e74e085185dd693ca4a25b13cd72
Nishan Chathuranga <inbox.nishan@gmail.com>
2020-02-06 09:02:59 +0530

    before revert

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ChartsModule } from 'ng2-charts';
+// import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,7 +23,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     
-    ChartsModule,
+    // ChartsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
